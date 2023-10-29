85d0805df2711fd0695b59080bf6ef72d5ed7059
denis <denis.safronov.228@gmail.com>
2023-10-30 00:02:49 +0300

    Lab4

--- app/src/main/java/com/example/lab2/Main_Active.java
+++ app/src/main/java/com/example/lab2/Main_Active.java
@@ -1,8 +1,11 @@
 package com.example.lab2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,6 +23,31 @@ import java.util.ArrayList;
 
 public class Main_Active extends Activity {
 
+    String username;
+    String password;
+
+
+    private SharedPreferences sharedPreferences;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(Main_Active.this, "onResume", Toast.LENGTH_SHORT).show();
+        TextView textView = findViewById(R.id.editTextText);
+
+        String savedInput = sharedPreferences.getString("text", "");
+        textView.setText(savedInput);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(Main_Active.this, "onPause", Toast.LENGTH_SHORT).show();
+        TextView textView = findViewById(R.id.editTextText);
+        String userInput = textView.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("text", userInput);
+        editor.apply();
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,40 +58,29 @@ public class Main_Active extends Activity {
         TextView textView = findViewById(R.id.editTextText);
         TextView password_line = findViewById(R.id.editTextNumberPassword);
 
+        sharedPreferences = getSharedPreferences("text", Context.MODE_PRIVATE);
 
+        /* SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("text", textView.getText().toString());
+        editor.apply();
+
+       button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(sharedPref.getString("text",""));
+            }
+        });*/
       //  File file = new File("C:\\Users\\denis\\AndroidStudioProjects\\Mobilki\\Mobilki\\app\\src\\main\\res\\datebase.txt");
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = textView.getText().toString();
-                String password = password_line.getText().toString();
+                username = textView.getText().toString();
+                password = password_line.getText().toString();
 
-                /*FileWriter writer;
-                try {
-                    writer = new FileWriter(file);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
 
-                // Записываем данные из EditText в файл
-                try {
-                    writer.append(username);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                // Закрываем FileWriter
-                try {
-                    writer.flush();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }*/
 
 
                 if (username.equals("admin1") && password.equals("123456")) {
